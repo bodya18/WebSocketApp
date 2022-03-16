eb633ff426c2b8c9fa2941cdc3560f940b846fef
bodya18 <bodya18x@mail.ru>
2022-03-16 15:04:22 +0300

    web

--- app.py
+++ app.py
@@ -1,29 +1,12 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+import asyncio
+import websockets
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+async def main():
+    async with websockets.serve(echo, "localhost", 23765):
+        await asyncio.Future()  # run forever
 
-@socketio.on('my event')
-def test_message(message):
-    emit('my response', {'data': message['data']})
-
-@socketio.on('my broadcast event')
-def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
-
-@socketio.on('connect')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
-
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
-
-if __name__ == '__main__':
-    socketio.run(app, port = 23765)
+asyncio.run(main())
